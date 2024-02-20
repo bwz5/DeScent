4a1e11b1c3a4e8af2b3c0208ee2349656db8637b
Benjamin Zou <bwz5@cornell.edu>
2024-02-19 20:46:12 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  printf("Welcome to OOP-DeScent ChipSat Software"); 
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
