64a74d7554521d8368206dd5b84f84e1ca9e414d
Benjamin Zou <bwz5@cornell.edu>
2024-02-19 21:49:42 -0500

    setting up sfr

---
+++ src/SensorReading.h
@@ -0,0 +1,22 @@
+#pragma once 
+
+/**
+ * Represents a sensor reading. Can update and get the current value. 
+*/
+class SensorReading {
+    private: 
+        /**
+         * Stores the current value 
+        */
+        float value; 
+    public: 
+        /**
+         * Returns the current value field 
+        */
+        float getCurrentValue(); 
+        /**
+         * Tries to update the current value. Returns true if updated and false if 
+         * there is not update. 
+        */
+        bool updateValue(); 
+}; 
