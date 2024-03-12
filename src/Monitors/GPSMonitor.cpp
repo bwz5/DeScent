400cff56d19718b5276d9772872f45cab2bc5f06
Benjamin Zou <bwz5@cornell.edu>
2024-03-11 21:57:20 -0400

    initialize tinygps branch

--- src/Monitors/GPSMonitor.cpp
+++ src/Monitors/GPSMonitor.cpp
@@ -3,23 +3,23 @@
 #include "sfr.h"
 
 void GPSMonitor::execute(){
-    sfr::GPS::longitude->setValue(gps.getLongitude()); 
-    sfr::GPS::latitude->setValue(gps.getLatitude()); 
+    // sfr::GPS::longitude->setValue(gps.getLongitude()); 
+    // sfr::GPS::latitude->setValue(gps.getLatitude()); 
     //sfr::GPS::altitude->setValue(gps.getAltitude());
 }
 
 GPSMonitor::GPSMonitor(){
     SPI.begin();
 
-    while (1) {
-        if (gps.begin(GPSCS) == false){
-            Serial.println("GPS not initialized, retrying..."); 
-        } else {
-            Serial.println("GPS Sensor is initialized"); 
-            initialized = true; 
-            break; 
-        }
-    }
+    // while (1) {
+    //     if (gps.begin(GPSCS) == false){
+    //         Serial.println("GPS not initialized, retrying..."); 
+    //     } else {
+    //         Serial.println("GPS Sensor is initialized"); 
+    //         initialized = true; 
+    //         break; 
+    //     }
+    // }
 }
 
 bool GPSMonitor::checkInitialization(){
--- src/Monitors/GPSMonitor.h
+++ src/Monitors/GPSMonitor.h
@@ -1,11 +1,11 @@
-#include <SparkFun_u-blox_GNSS_v3.h>
+
 
 #define GPSCS 1// the Chip select pin
 
 class GPSMonitor {
     private:
         bool initialized = false; 
-        SFE_UBLOX_GNSS_SPI gps; 
+        
     public: 
         /**
          * Begin reading from the sensor
